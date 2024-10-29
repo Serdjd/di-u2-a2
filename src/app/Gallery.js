33bd73288123416a5d71b96dc1b6c37208b5844a
Serdjd <sergiodobladomunoz@gmail.com>
2024-10-29 13:53:34 +0100

    first commit

---
+++ src/app/Gallery.js
@@ -0,0 +1,66 @@
+import { getImageUrl } from './utils.js';
+const curie = {
+  url:'szV5sdG',
+  name: "Maria Skłodowska-Curie",
+  profesion: {
+    name: "física y química",
+    numPremios: 4,
+    premios: "Premio Nobel de Física, Premio Nobel de Química, Medalla Davy, Medalla Matteucci",
+    descubrimientos: "polonio (elemento químico)"
+  }
+}
+const katsuko = {
+  url:'YfeOqp2',
+  name: "Katsuko Saruhashi",
+  profesion: {
+    name: "geoquímica",
+    numPremios: 2,
+    premios: "Premio Miyake de geoquímica, Premio Tanaka",
+    descubrimientos: "un método para medir el dióxido de carbono en el agua de mar"
+  }
+}
+
+function Profile({persona}) {
+  return (
+    <section className="profile">
+      <h2>{persona.name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(persona.url)}
+        alt={persona.name}
+        width={70}
+        height={70}
+      />
+      <Work profesion = {persona.profesion} />
+    </section>
+  )
+
+}
+function Work({profesion}) {
+  return (
+    <ul>
+      <li>
+        <b>Profesión: </b> 
+        {profesion.name}
+      </li>
+      <li>
+        <b>Premios: {profesion.numPremios} </b>
+        ({profesion.premios})
+      </li>
+      <li>
+        <b>Descubrió: </b>
+        {profesion.descubrimientos}
+      </li>
+    </ul>
+  )
+}
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1>Científicos Notables</h1>
+      <Profile persona = {curie} />
+      <Profile persona = {katsuko} />
+    </div>
+  );
+}
